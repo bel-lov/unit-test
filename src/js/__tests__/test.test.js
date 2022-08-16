d40dfdf512dd4c870233ec2ebbdb05c268a5e37b
bel-lov <belou.lyuba@yandex.ru>
2022-08-16 18:32:18 +0300

    second commit

--- src/js/__tests__/test.test.js
+++ src/js/__tests__/test.test.js
@@ -3,27 +3,27 @@ import calculateLife from '../app.js';
 // let test;
 // let expect;
 test('calculateLifeGreen', () => {
-    const person = {
-        name: 'Маг',
-        health: 90,
-    };
-    const result = calculateLife(person);
-    expect(result).toBe('green');
+  const person = {
+    name: 'Маг',
+    health: 90,
+  };
+  const result = calculateLife(person);
+  expect(result).toBe('green');
 });
 test('calculateLifeYellow', () => {
-    const person = {
-        name: 'Маг',
-        health: 40,
-    };
-    const result = calculateLife(person);
-    expect(result).toBe('yellow');
+  const person = {
+    name: 'Маг',
+    health: 40,
+  };
+  const result = calculateLife(person);
+  expect(result).toBe('yellow');
 });
 
 test('calculateLifeRed', () => {
-    const person = {
-        name: 'Маг',
-        health: 10,
-    };
-    const result = calculateLife(person);
-    expect(result).toBe('red');
+  const person = {
+    name: 'Маг',
+    health: 10,
+  };
+  const result = calculateLife(person);
+  expect(result).toBe('red');
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
 export default function calculateLife(lifes) {
-    let result;
-    if (lifes.health > 50) {
-        result = 'green';
-    }
-    if (lifes.health < 50 && lifes.health > 15) {
-        result = 'yellow';
-    }
-    if (lifes.health < 15) {
-        result = 'red';
-    }
-    return result;
+  let result;
+  if (lifes.health > 50) {
+    result = 'green';
+  }
+  if (lifes.health < 50 && lifes.health > 15) {
+    result = 'yellow';
+  }
+  if (lifes.health < 15) {
+    result = 'red';
+  }
+  return result;
 }
